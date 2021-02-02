3ea592c78dddeacb1478ebf536f09da6a08ed794
NeilMajumdar19 <neil.majumdar19@gmail.com>
2021-02-02 03:55:10 -0800

    added path for getAirplaneById

--- src/main/java/com/smoothstack/spring/utopia/controller/AirplaneController.java
+++ src/main/java/com/smoothstack/spring/utopia/controller/AirplaneController.java
@@ -4,6 +4,9 @@ import com.smoothstack.spring.utopia.entity.Airplane;
 import com.smoothstack.spring.utopia.entity.AirplaneType;
 import com.smoothstack.spring.utopia.service.AirplaneService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +17,8 @@ public class AirplaneController {
     @Autowired
     private AirplaneService airplaneService;
 
-    public Airplane getAirplaneById(int planeId)
+    @RequestMapping(method = RequestMethod.GET, path = "/airplanes/{planeId}")
+    public Airplane getAirplaneById(@PathVariable int planeId)
     {
         return airplaneService.getAirplaneById(planeId);
     }
